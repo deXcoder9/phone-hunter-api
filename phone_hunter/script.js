5ec16fb6c1e976d489f7e7c99154d171c012fa93
deXcoder9 <dhrubothejuniorprogrammer@gmail.com>
2024-02-29 14:56:19 +0600

    view all task complete

--- phone_hunter/script.js
+++ phone_hunter/script.js
@@ -1,23 +1,26 @@
 console.log("javascript codes loaded");
 
-const fetchingURL = async (seachPhone) => {
+const fetchingURL = async (seachPhone, isViewAll) => {
   const response = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${seachPhone}`
   );
   const data = await response.json();
   let singlePhone = data.data;
-  displyingPhones(singlePhone);
+  displyingPhones(singlePhone, isViewAll);
 };
 
-function displyingPhones(pdata) {
+function displyingPhones(pdata, isViewAll) {
   let totalPhones = pdata.length;
   const showButton = document.getElementById("showALlButton");
-  if (pdata.length > 10) {
+  if (pdata.length > 10 && !isViewAll) {
     showButton.classList.remove("hidden");
   } else {
     showButton.classList.add("hidden");
   }
-  pdata = pdata.slice(0, 10);
+  console.log("show all is ", isViewAll);
+  if (!isViewAll) {
+    pdata = pdata.slice(0, 10);
+  }
   console.log(pdata);
 
   const phoneContainer = document.getElementById("phoone_container");
@@ -44,11 +47,27 @@ function displyingPhones(pdata) {
         </div>
     `;
     phoneContainer.appendChild(phoneCard);
+    loading(false);
   }
 }
 
-function specificPhones() {
+function specificPhones(isViewAll) {
   const inputfield = document.getElementById("searchINput");
   const inputfieldValue = inputfield.value;
-  fetchingURL(inputfieldValue);
+  fetchingURL(inputfieldValue, isViewAll);
+  loading(true);
 }
+
+const loading = (e) => {
+  const loading_conatiner = document.getElementById("loadingCOntainer");
+  if (e) {
+    loading_conatiner.classList.remove("hidden");
+  } else {
+    loading_conatiner.classList.add("hidden");
+  }
+};
+
+// show all button
+const displyingShowAll = () => {
+  specificPhones(true);
+};
