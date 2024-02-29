01b895c34cf31366ae960c6262fa9044badf7c25
deXcoder9 <dhrubothejuniorprogrammer@gmail.com>
2024-02-29 19:08:43 +0600

    phone details feature added

--- phone_hunter/script.js
+++ phone_hunter/script.js
@@ -1,6 +1,6 @@
-console.log("javascript codes loaded");
+// console.log("javascript codes loaded");
 
-const fetchingURL = async (seachPhone, isViewAll) => {
+const fetchingURL = async (seachPhone = "iPhone", isViewAll) => {
   const response = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${seachPhone}`
   );
@@ -17,11 +17,11 @@ function displyingPhones(pdata, isViewAll) {
   } else {
     showButton.classList.add("hidden");
   }
-  console.log("show all is ", isViewAll);
+  // console.log("show all is ", isViewAll);
   if (!isViewAll) {
     pdata = pdata.slice(0, 10);
   }
-  console.log(pdata);
+  // console.log(pdata);
 
   const phoneContainer = document.getElementById("phoone_container");
   phoneContainer.textContent = " ";
@@ -41,7 +41,7 @@ function displyingPhones(pdata, isViewAll) {
             <p>There are many variations of passages of available, but the majority have suffered</p>
             <h4 class='text-2xl my-3'>$999 </h4> 
             <div class="card-actions">
-              <button class="btn btn-primary">Buy Now</button>
+              <button onclick="showDetails('${phone.slug}') , phoneDetails.showModal()" class="btn btn-primary">Show Details</button>
             </div>
           </div>
         </div>
@@ -71,3 +71,40 @@ const loading = (e) => {
 const displyingShowAll = () => {
   specificPhones(true);
 };
+
+// phone show details
+const showDetails = async (id) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const data = await response.json();
+
+  // console.log("id", id);
+  console.log("data-", data.data);
+  let details = data.data;
+  console.log("data-", details.mainFeatures.storage);
+  const PhoneDetails = document.getElementById("Phone__details");
+  // console.log(details_container);
+  PhoneDetails.innerHTML = `
+  <div id="Phone__details" class="">
+            <div class="flex flex-col justify-center items-center">
+              <img
+                src="https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro.jpg"
+                alt=""
+              />
+            </div>
+            <h3 class="font-bold text-[25px] pt-2 pb-5">${details.name}</h3>
+            <p class="py-2"><b>Storage:</b> ${details.mainFeatures.storage}</p>
+            <p class="py-2"><b>Display Size:</b> ${details.mainFeatures.displaySize}  </p>
+            <p class="py-2"><b>Chipset:</b> ${details.mainFeatures.chipSet}</p>
+            <p class="py-2"><b>Memory:</b> ${details.mainFeatures.memory}</p>
+            <p class="py-2"><b>Slug:</b> ${details.slug}</p>
+            <p class="py-2"><b>Release data:</b> ${details.releaseDate}</p>
+            <p class="py-2"><b>Brand:</b> ${details.brand}</p>
+            <p class="py-2"><b>GPS:</b> ${details.others.GPS}</p>
+          </div> 
+  
+  `;
+};
+
+fetchingURL();
